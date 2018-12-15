248f6864d574f99c9321867a2909f8a824013655
Maja Vrsaljko <majavrsaljko78@gmail.com>
2018-12-15 18:08:47 +0100

    dbconnect spaja php fileove sa bazom, ubuduće ćemo ga uključiti u zaglavlje svakog php-a.

---
+++ dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+/*podaci o serveru i tvoj username
+imamo dva načina za stvoriti konekciju
+mysqli (object-oriented) sa new
+mysli (procedural)
+više mi se sviđa procedural tako da fak you iva
+*/
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "grobenk";
+
+//stvaram konekciju
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if(!$conn){
+    die("Connection failed: " .mysqli_connect_error());
+}
+else{
+    echo ("Faking konektovani");
+}
+?>
