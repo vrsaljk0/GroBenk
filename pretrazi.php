cad8919cb6347da4abfefe3db386d3ac1a778f87
Maja Vrsaljko <majavrsaljko78@gmail.com>
2018-12-16 17:07:02 +0100

    Pretraga izbaci rezultate ko linkove koji bi trebali voditi na public profile ljudi. to još nisam

---
+++ pretrazi.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "dbconnect.php";
+    session_start();
+
+    $pretraga = $_GET['trazilica'];
+
+    $query = "SELECT ime_prezime_donora, OIB_donora from donor WHERE (lower(ime_prezime_donora) LIKE '$pretraga%')";
+    $run = mysqli_query($conn, $query);
+    $result = $run or die ("Failed to query database". mysql_error());
+
+    $post = array();
+    while($row = mysqli_fetch_array($result)){
+          echo '<a href="publicprofile.php">'.$row['ime_prezime_donora'].'</a><br>';
+    }
+
+    if($run){
+        echo "Uspjesna pretraga";
+    }
+    else {
+        echo "error:" .mysqli_error($conn);
+    }
+?>
