3901ce6bd221f0fa0897ec288a8ec6dba6a75a97
Maja Vrsaljko <majavrsaljko78@gmail.com>
2019-01-03 22:49:42 +0100

    javni profil bolnica

---
+++ publicbolnica.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maja Vrsaljko
+ * Date: 3.1.2019.
+ * Time: 11:39
+ */
+
+  require_once ("dbconnect.php");
+  session_start();
+  $id_bolnice = $_GET['bolnica'];
+  $OIB = $_SESSION['mojOIB'];
+
+//   $sql = "SELECT *from bolnica WHERE idbolnica=''";
+
+?>
+
