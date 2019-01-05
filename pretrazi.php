7ead24e59101cd5a037be9671b567607ee4f68d6
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-01-05 15:08:16 +0100

    Add files via upload

--- pretrazi.php
+++ pretrazi.php
@@ -2,27 +2,63 @@
     require_once "dbconnect.php";
     session_start();
     $pretraga = $_GET['trazilica'];
+    $OIB = $_SESSION['mojOIB'];
     //echo $_SESSION['mojOIB'];
+
+    echo '
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BloodBank</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+        <link href="style.css" rel="stylesheet">
+        <link href="donorstyle.css" rel="stylesheet">
+    </head>';
+
+    echo "
+    <div id='nav-placeholder' onload>
+    </div> 
+
+    <script>
+    $(function(){
+      $('#nav-placeholder').load('donornavbar.php');
+    });
+    </script>";
 
     /**
      * Pretraga donora
     */
     $donor = 0;
-    echo'<p><b>Donori:</b></p>';
-    $query = "SELECT *from donor WHERE (prebivaliste LIKE '%$pretraga%') OR (ime_prezime_donora LIKE '%$pretraga%')";
+    
+    $query = "SELECT *from donor WHERE (prebivaliste LIKE '%$pretraga%') OR (ime_prezime_donora LIKE '%$pretraga%') AND OIB_donora!='$OIB'";
     $run = mysqli_query($conn, $query);
     $result = $run or die ("Failed to query database". mysqli_error($conn));
 
+    echo '<h4 class="pretraga-title">Rezultati pretrage:</h4>';
+
+    echo '<div class="pretraga">';
     while($row = mysqli_fetch_array($result)){
         //echo '<a href="publicprofile.php">'.$row['ime_prezime_donora'].'</a><br>';
-        echo '<a href="publicprofile.php?OIB_korisnika='.urlencode($row['OIB_donora']).'">'.$row['ime_prezime_donora'].'</a><br>';
+        echo '
+            <div class="pretraga-info">
+                <div class="pretraga-img">
+                    <img src="donori/'.$row['image'].'">
+                </div>
+                <a href="publicprofile.php?OIB_korisnika='.urlencode($row['OIB_donora']).'">'.$row['ime_prezime_donora'].'</a>
+            </div><br>
+        ';
         $donor = 1;
     }
+     echo '</div>';
     /**
      * Pretraga bolnica
      */
     $bolnica = 0;
-    echo '<p><b>Bolnice:</b></p>';
+    
     $query = "SELECT *from bolnica WHERE (grad LIKE '%$pretraga%') OR (naziv_bolnice LIKE '%$pretraga%')";
     $run = mysqli_query($conn, $query);
     $result = $run or die ("Failed to query database". mysqli_error($conn));
@@ -32,5 +68,5 @@
         echo '<a href="publicbolnica.php?id_bolnice='.urlencode($row['idbolnica']).'">'.$row['naziv_bolnice'].'</a><br>';
         $bolnica = 1;
     }
-    if(!$bolnica && !$donor) echo'Nazalost nema rezultata pretrage.';
+    if(!$bolnica && !$donor) echo'Nažalost nema rezultata pretrage.';
 ?>
