9a69d6cf428c62a958d1f5520d2760bc0bc440e7
Maja Vrsaljko <majavrsaljko78@gmail.com>
2019-01-07 20:11:58 +0100

    upit koji označuje notifikacije kao pročitane (status postavi na 1). Morala sam to staviti u odvojeni php jer mi u navbaru nije radilo (on se stalno refreša pa se izgube vrijednosti)

---
+++ notification.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Korisnik
+ * Date: 7.1.2019.
+ * Time: 16:04
+ */
+
+require_once "dbconnect.php";
+
+    $OIB = $_POST['OIB'];
+    if (!empty($_POST['check_list'])) {
+        foreach ($_POST['check_list'] as $id) {
+            echo $id;
+            $sql = "UPDATE obavijesti SET status='1' WHERE id_obavijesti='$id'";
+            $run = mysqli_query($conn, $sql);
+            $result = $run or die ("Failed to query database". mysqli_error($conn));
+        }
+    }
+    $url = 'donor.php?OIB='.urlencode($OIB);
+    header("Location:$url");
+
+
+
+
+?>
