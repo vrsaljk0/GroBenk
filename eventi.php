1fef004d4081c1003e9f37c37cf6fcd63b1f545d
Bachvac <ivanaxbacac@gmail.com>
2019-01-29 00:18:23 +0100

    kad se oba radiobuttona oznace se sjebalo + promjenila izgled malo

--- eventi.php
+++ eventi.php
@@ -78,40 +78,40 @@ echo '
         </ul>
 </div>';
 
-    if(isset($_POST['delete'])){
-        if (!empty($_POST['check_list'])) {
-            foreach ($_POST['check_list'] as $id) {
-                $sql = "DELETE from lokacija WHERE id_lokacije ='$id'";
-                $run = mysqli_query($conn, $sql);
-                $result = $run or die ("Failed to query database". mysqli_error($conn));
+if(isset($_POST['delete'])){
+    if (!empty($_POST['check_list'])) {
+        foreach ($_POST['check_list'] as $id) {
+            $sql = "DELETE from lokacija WHERE id_lokacije ='$id'";
+            $run = mysqli_query($conn, $sql);
+            $result = $run or die ("Failed to query database". mysqli_error($conn));
 
-            }
         }
     }
-    if(isset($_POST['submit_event'])){
-        $datum = $_POST['datum'];
-        $grad = $_POST['grad'];
-        $lokacija = $_POST['lokacija'];
-        $adresa = $_POST['adresa'];
-        $postanski_broj = $_POST['postbroj'];
-        $start = $_POST['startt'];
-        $kraj = $_POST['kraj'];
-        $image = $_FILES['image']['name'];
-        $target = "lokacije/".basename($image);
-        $filename = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
+}
+if(isset($_POST['submit_event'])){
+    $datum = $_POST['datum'];
+    $grad = $_POST['grad'];
+    $lokacija = $_POST['lokacija'];
+    $adresa = $_POST['adresa'];
+    $postanski_broj = $_POST['postbroj'];
+    $start = $_POST['startt'];
+    $kraj = $_POST['kraj'];
+    $image = $_FILES['image']['name'];
+    $target = "lokacije/".basename($image);
+    $filename = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
 
-        $query = "INSERT into lokacija (grad, naziv_lokacije, adresa_lokacije, postanski_broj, datum_dogadaja, start, kraj, image) values ('$grad', '$lokacija', '$adresa', '$postanski_broj','$datum', '$start', '$kraj', '$image')";
-        $run = mysqli_query($conn, $query);
-        $result = $run or die ("Failed to query database" . mysqli_error($conn));
+    $query = "INSERT into lokacija (grad, naziv_lokacije, adresa_lokacije, postanski_broj, datum_dogadaja, start, kraj, image) values ('$grad', '$lokacija', '$adresa', '$postanski_broj','$datum', '$start', '$kraj', '$image')";
+    $run = mysqli_query($conn, $query);
+    $result = $run or die ("Failed to query database" . mysqli_error($conn));
 
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
-            $msg = "Podaci uspješno promijenjeni";
-        } else {
-            $msg = "Došlo je do greške";
-        }
-
-        header("Location:eventi.php?keyword=&trazi=Traži");
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+        $msg = "Podaci uspješno promijenjeni";
+    } else {
+        $msg = "Došlo je do greške";
     }
+
+    header("Location:eventi.php?keyword=&trazi=Traži");
+}
 
 echo '
 <div class="eventi-table">
@@ -119,7 +119,7 @@ echo '
         <table class="event-t">
         <br><span class="newevent">Dodaj novi event:</span>
             <tr>
-                <th style="border-left: 2px solid #9F0A00;" class="tht">DATUM</th>
+                <th style="border-left: 2px solid #9F0A00;" class="tht";>DATUM</th>
                 <th class="tht">GRAD</th>
                 <th class="tht">LOKACIJA</th>
                 <th class="tht">ADRESA</th>
@@ -127,18 +127,19 @@ echo '
                 <th class="tht">POČINJE</th>
                 <th class="tht">ZAVRŠAVA</th>
                 <th style="border-right: 2px solid #9F0A00;" class="tht">SLIKA</th>
+                <th style="border-right: 2px solid #9F0A00;" class="tht">  </th>
             </tr>
             <tr>
                 <td style="border-left: 2px solid #9F0A00;"><input type="date" name = "datum" required=""></td>
                 <td class="eventi-td1"><input type="text" name = "grad" required=""></td>
                 <td><input type="text" name = "lokacija" required=""></td>
                 <td><input type="text" name = "adresa" required=""></td>
-                <td class="eventi-td1"><input type="number" name = "postbroj" required=""></td>
+                <td><input type="number" name = "postbroj" required="" style="width: 90px"></td>
                 <td><input type="time" name = "startt" required=""></td>
                 <td><input type="time" name = "kraj" required=""></td>
-                <td style="border-right:2px solid #9F0A00;"><input type="file" name = "image" class="form-control"></td><br><br>
+                <td style="border-right:2px solid #9F0A00;" style="width: 50px"><input type="file" name = "image" class="form-control"></td>
                 <input type="hidden" name="image_text" value="image_text">
-                <td style="border-style: none; padding-left: 10px;"><input type="submit" class="zbtn" name="submit_event" value="Dodaj event"></td>
+                <td style="border-style: none; padding-left: 10px;" ><input type="submit" class="zbtn" name="submit_event" value="Dodaj event"></td>
             </tr>
         </table>
     </form>
@@ -148,12 +149,12 @@ echo '
         <input type="text" class="eventi-pretrazi" name = "keyword" placeholder="Pretraži evente">
         <span class="eventi-radio">
             <label class="containerreg"><span class="prevent">Nadolazeći eventi</span>
-                <input type="radio" name="buduci" value="buduci">
+                <input type="radio" name="radio" value="buduci">
                 <span class="checkmarkreg"></span>
             </label>
 
             <label class="containerreg"><span class="prevent">Prošli eventi</span>
-                <input type="radio" name="prosli" value="prosli"">
+                <input type="radio" name="radio" value="prosli"">
                 <span class="checkmarkreg"></span>
             </label>
         </span>
@@ -163,54 +164,53 @@ echo '
 
 
     <form action="" method="POST">
-    <div class="back">
     <table style="margin-left:0px; width:100%; overflow:hidden;" class="table table-fixed">
         <thead class="t">
             <tr>
                 <th style="width:10%;" class="thh">DATUM</th>
-                <th class="thh">GRAD</th>
+                <th class="thh" style = "width: 130px">GRAD</th>
                 <th class="thh">LOKACIJA</th>
                 <th class="thh">ADRESA</th>
                 <th class="thh">POŠTANSKI BROJ</th>
                 <th style="width:8%;" class="thh">POČINJE</th>
                 <th style="width:8%;" class="thh">ZAVRŠAVA</th>
-                <th class="thh">PODACI O EVENTU</th>
+                <th class="thh" style = "width: 180px">PODACI O EVENTU</th>
                 <th style="width:9%;" class="thh">IZBRIŠI</th>
             </tr>
         </thead>';
 
 
 
-            if(isset($_GET['trazi'])) {
-            $radio = 0;
-            $pretraga = $_GET['keyword'];
-            $datum = date('Y-m-d');
-            if(isset($_GET['buduci']) && $pretraga ==''){
-                $query = "SELECT *from lokacija WHERE datum_dogadaja > '$datum' order by id_lokacije desc";
-            }
-           else if(isset($_GET['buduci']) && $pretraga !=''){
-               $query = "SELECT *from lokacija WHERE ((grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%')) AND (datum_dogadaja > '$datum') order by id_lokacije desc";
-           }
-            else if(isset($_GET['prosli']) && $pretraga !=''){
-                $query = "SELECT *from lokacija WHERE ((grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%')) AND (datum_dogadaja < '$datum') order by id_lokacije desc";
-            }
-            else if(isset($_GET['prosli']) && $pretraga ==''){
-                $query = "SELECT *from lokacija WHERE datum_dogadaja < '$datum' order by id_lokacije desc";
-            }
-            else{
-                $query = "SELECT *from lokacija WHERE (grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%') order by id_lokacije desc";
-            }
+if(isset($_GET['trazi'])) {
+    $radio = 0;
+    $pretraga = $_GET['keyword'];
+    $datum = date('Y-m-d');
+    if(isset($_GET['radio']) and ($_GET['radio'])=='buduci' && $pretraga ==''){
+        $query = "SELECT *from lokacija WHERE datum_dogadaja > '$datum' order by id_lokacije desc";
+    }
+    else if(isset($_GET['radio'])and ($_GET['radio'])=='buduci' && $pretraga !=''){
+        $query = "SELECT *from lokacija WHERE ((grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%')) AND (datum_dogadaja > '$datum') order by id_lokacije desc";
+    }
+    else if(isset($_GET['radio'])and ($_GET['radio'])=='prosli' && $pretraga !=''){
+        $query = "SELECT *from lokacija WHERE ((grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%')) AND (datum_dogadaja < '$datum') order by id_lokacije desc";
+    }
+    else if(isset($_GET['radio'])and ($_GET['radio'])=='prosli' && $pretraga ==''){
+        $query = "SELECT *from lokacija WHERE datum_dogadaja < '$datum' order by id_lokacije desc";
+    }
+    else{
+        $query = "SELECT *from lokacija WHERE (grad LIKE '%$pretraga%') OR (naziv_lokacije LIKE '%$pretraga%') OR (adresa_lokacije LIKE '%$pretraga%') OR (postanski_broj LIKE '%$pretraga%') OR (datum_dogadaja LIKE '%$pretraga%') order by id_lokacije desc";
+    }
 
-            $run = mysqli_query($conn, $query);
-            $result = $run or die ("Failed to query database" . mysqli_error($conn));
-            echo '<div>';
-                while ($row = mysqli_fetch_array($result)) {
-                    $hours_start = date('H', strtotime($row['start']));
-                    $minutes_start = date('i', strtotime($row['start']));
-                    $hours_kraj = date('H', strtotime($row['kraj']));
-                    $minutes_kraj = date('i', strtotime($row['kraj']));
-                    if ($row['datum_dogadaja'] > $datum) {
-                        echo '
+    $run = mysqli_query($conn, $query);
+    $result = $run or die ("Failed to query database" . mysqli_error($conn));
+    echo '<div>';
+    while ($row = mysqli_fetch_array($result)) {
+        $hours_start = date('H', strtotime($row['start']));
+        $minutes_start = date('i', strtotime($row['start']));
+        $hours_kraj = date('H', strtotime($row['kraj']));
+        $minutes_kraj = date('i', strtotime($row['kraj']));
+        if ($row['datum_dogadaja'] > $datum) {
+            echo '
                         <tr bgcolor="#e6ffe6">
                             <td style="width:10%;" class="trr">' . $row['datum_dogadaja'] . '</td>
                             <td class="trr"> ' . $row['grad'] . '</td>
@@ -222,8 +222,8 @@ echo '
                             <td><a href = "edit_event.php?idEvent='. $row["id_lokacije"].'">Uredi</a></td>
                             <td style="float:right; width:9%;" class="trr"><input type="checkbox" style="margin-left:40px;" class="case" name="check_list[]" value='.$row['id_lokacije'].' ></td>
                         </tr>';
-                    } else {
-                        echo '
+        } else {
+            echo '
                         <tr>
                             <td style="width:10%;" class="trr">' . $row['datum_dogadaja'] . '</td>
                             <td class="trr"> ' . $row['grad'] . '</td>
@@ -235,12 +235,12 @@ echo '
                             <td><a href = "show_event.php?idEvent='. $row["id_lokacije"].'">Prikaži detaljnije</a></td>
                             <td></td>
                         </tr>';
-                    }
-                }
-             }
-                echo'
+        }
+    }
+}
+echo'
                 
-            </table></div><br>
+            </table><br>
                 <span class="prevent">Označi sve&nbsp</span>
                 <input type="checkbox" name="select_all" id = "select_all"><br><br>
                 <input type="submit" class="zbtn" name="delete" value="Izbriši evente">
