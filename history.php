300c438b25e35aa66e844264b1f4125961ceb8e0
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-01-29 18:29:25 +0100

    CHAT-zadnje obavijesti, zadnji datum i osposobit tražilicu

--- history.php
+++ history.php
@@ -1,7 +1,18 @@
 <?php
+require_once "dbconnect.php";
+require_once "functions.php";
+session_start();
+mysqli_set_charset($conn,"utf8");
+/** SESSION TIMEOUT */
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+    header("Location:odjava.php");
+}
+$_SESSION['LAST_ACTIVITY'] = time();
 
+if (!isset($_SESSION['donor_loggedin'])) header("Location:denied_permission.php");
 
 echo '
+<html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,21 +24,17 @@ echo '
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <link href="style.css" rel="stylesheet">
     <link href="donorstyle.css" rel="stylesheet">
-</head>';
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
+    <link href="tmp.css" rel="stylesheet">
+</head>
 
-    require_once "dbconnect.php";
-    require_once "functions.php";
-    session_start();
-    mysqli_set_charset($conn,"utf8");
-    /** SESSION TIMEOUT */
-    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
-        header("Location:odjava.php");
-    }
-    $_SESSION['LAST_ACTIVITY'] = time();
 
-    if (!isset($_SESSION['donor_loggedin'])) header("Location:denied_permission.php");
-
-    echo "
+</head>
+<body>';
+echo "
     <div id='nav-placeholder' onload>
     </div> 
 
@@ -43,24 +50,70 @@ $run = mysqli_query($conn, $info);
 $result = $run or die ("Failed to query database". mysqli_error($conn));
 $row = mysqli_fetch_array($result);
 $moje_ime = $row['ime_prezime_donora'];
-echo 'ISPIS SVIH PORUKA<br><br><br>';
 
 $sql = "SELECT * from obavijesti where OIBdonora ='$OIB' and ID_posiljatelja !='1'group by ID_posiljatelja";
 $run = mysqli_query($conn, $sql);
 $result = $run or die ("Failed to query database". mysqli_error($conn));
 
-echo'  <div class="profil-img">
-            <a href="admin_history.php">ADMIN</a><br><br>';
-            while($row = mysqli_fetch_array($result)){
-                $OIB_prijatelja = $row['ID_posiljatelja'];
-                $prijatelj = "SELECT * from donor where OIB_donora = '$OIB_prijatelja'";
-                $run_prijatelj = mysqli_query($conn, $prijatelj);
-                $result2 = $run_prijatelj or die ("Failed to query database". mysqli_error($conn));
-                $row_prijatelj = mysqli_fetch_array($result2);
-                $ime = $row_prijatelj['ime_prezime_donora'];
-                $username_prijatelja = $row_prijatelj['username'];
-            echo '<a href="user_history.php?username='.urlencode($username_prijatelja).'"><font color="FF00CC">'.$row_prijatelj['ime_prezime_donora'].'</font></a><br><br>';
-            }
-         echo'</div>   
-    </div>';
+echo '
+<div class="container">
+<div class="messaging">
+      <div class="inbox_msg">
+        <div class="inbox_people">
+          <div class="headind_srch">
+            <div class="recent_heading">
+              <h4>Povijest poruka</h4>
+            </div>
+            <div class="srch_bar">
+              <div class="stylish-input-group">
+                <input type="text" class="search-bar"  placeholder="Pretraži" >
+                <span class="input-group-addon">
+                <button type="button"> <i class="fa fa-search" aria-hidden="true"></i> </button>
+                </span> </div>
+            </div>
+          </div>
+          <div class="inbox_chat">
+            <div class="chat_list">
+
+                <a href="admin_history.php">
+                <div class="chat_people">
+                    <div class="chat_img"> <img src="donori/admin.png"> </div>
+                    <div class="chat_ib">
+                      <h5>Admin <span class="chat_date">Dec 25</span></h5>
+                      <p>Test, which is a new approach to have all solutions 
+                        astrology under one roof.</p>
+                    </div>
+                </div>
+                </a><br>';
+
+
+while($row = mysqli_fetch_array($result)){
+    $OIB_prijatelja = $row['ID_posiljatelja'];
+    $prijatelj = "SELECT * from donor where OIB_donora = '$OIB_prijatelja'";
+    $run_prijatelj = mysqli_query($conn, $prijatelj);
+    $result2 = $run_prijatelj or die ("Failed to query database". mysqli_error($conn));
+    $row_prijatelj = mysqli_fetch_array($result2);
+    $ime = $row_prijatelj['ime_prezime_donora'];
+    $username_prijatelja = $row_prijatelj['username'];
+    echo '
+                <a class="a" href="user_history.php?username='.urlencode($username_prijatelja).'">
+                    <div class="chat_people">
+                        <div class="chat_img"> <img src="'.$row_prijatelj['image'].'"> </div>
+                        <div class="chat_ib">
+                          <h5>'.$row_prijatelj['ime_prezime_donora'].'<span class="chat_date">Dec 25</span></h5>
+                          <p>Test, which is a new approach to have all solutions 
+                            astrology under one roof.</p>
+                        </div>
+                    </div>
+                </a><br>';
+}
+
+echo '
+            </div>
+          </div>
+        </div>
+      </div>
+    </div></div>
+    </body>
+    </html>';
 ?>
