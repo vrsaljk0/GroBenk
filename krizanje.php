c443016ae781803d0fee3e4c94e7d43171dd369d
Maja Vrsaljko <majavrsaljko78@gmail.com>
2019-01-29 23:33:52 +0100

    POST metoda ZA LOGIN!!!!

--- krizanje.php
+++ krizanje.php
@@ -12,8 +12,8 @@ mysqli_set_charset($conn,"utf8");
 
 session_start();
 
-$username = $_GET['username'];
-$password = $_GET['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
 $username = stripcslashes($username);
 $password = stripcslashes($password);
