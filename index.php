2b2ac902f30c3dab9abc3bce9ef508e50852be12
Azra Subašić <44138037+hax91@users.noreply.github.com>
2019-01-30 22:16:36 +0100

    ubrzan slide

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once "dbconnect.php";
 session_start();
 mysqli_set_charset($conn,"utf8");
-
+$_SESSION["vratime"] = $_SERVER['REQUEST_URI'];
 
 ?>
 
@@ -32,7 +32,7 @@ mysqli_set_charset($conn,"utf8");
 </script>
 
 <!--- Image Slider -->
-<div id="slides" class="carousel slide" data-ride="carousel">
+<div id="slides" class="carousel slide" data-ride="carousel" data-interval="2000">
     <ul class="carousel-indicators">
         <li data-target="#slides" data-slide-to="0" class="active"></li>
         <li data-target="#slides" data-slide-to="1" ></li>
@@ -44,20 +44,14 @@ mysqli_set_charset($conn,"utf8");
             <div class="carousel-caption">
                 <h1>Želite li biti donor krvi?</h1>
                 <h3>Postanite dio naše zajednice!</h3>
-                <button type="button" class="btn btn-outline-light btn-lg" onclick="document.getElementById('id01').style.display='block'"">SIGN UP</button>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="img/slide2.jpg">
-            <div class="carousel-caption">
-                <h1>Trenutne zalihe krvi</h1>
+                <a href="signup.php"><button type="button" class="btn btn-outline-light btn-lg">SIGN UP</button></a>
             </div>
         </div>
         <div class="carousel-item">
             <img src="img/slide3.jpg">
             <div class="carousel-caption">
                 <h1>Potrebna vam je krv?</h1>
-                <button type="button" class="btn btn-outline-light btn-lg">Pošalji zahtjev</button>
+                <h3>Pošaljite nam e-mail na info@grobenk.hr</h3>
             </div>
         </div>
     </div>
@@ -69,86 +63,75 @@ mysqli_set_charset($conn,"utf8");
 <div class="border">
     <div class="container-fluid">
         <div class="row jumbotron">
-            <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
-                <p class="lead">BLablablalibla bla nesto o krvi i donorima and shit</p>
+            <div style="width:65%;float:left">
+                <h1>Doniraj i spasi život!</h1><br>
+                <p class="lead" style="font-size: 20px">Pridružite se GroBenk zajednici darivatelja krvi.<br>
+                    <ul style="margin-left: 30px">
+                        <li style="font-size: 18px">Budite informirani o organiziranim akcijama darivanja krvi u vašoj okolici</li>
+                        <li style="font-size: 18px">Podijelite svoja iskustva</li>
+                        <li style="font-size: 18px">Povežite se s ostalim donorima</li>
+                        <li style="font-size: 18px">Pratite povijest svojih donacija</li>
+                    </ul>
+                </p>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
-                <a href="#"><button type="button" class="btn btn-outline-secondary btn-lg">Možda i tu sign in</button></a>
+            <div style="width:35%;float:right">
+                <img src="img/donatorifrendici.jpg">
             </div>
         </div>
-    </div>
+    </div><br><br>
 
     <!--- Welcome Section -->
-    <div class="container-fluid padding">
-        <div class="row welcome text-center">
-            <div class="col-12">
-                <h1 class="display-4">Doniraj s razlogom.</h1>
-                <hr>
-            </div>
-            <div class="col-12">
-                <p class="lead">Dobrodosli na nasu donorsku stranicu yoo supp ma man</p>
-            </div>
+    <div>
+        <div style="position:absolute;width:26%;float:right;background-color:#e9ecef;padding:30px;border-radius:8px;">
+            <h1>Trenutne zalihe krvi</h1><br>
+            <p style="font-size: 17px">
+                Na slikama prikazano je trenutačno stanje zaliha krvi prema krvnim grupama. <br><br>
+                Optimalne količine su između vrijednosti označene kao <font style="color: #A60202">PREMALE</font> i <font style="color: #A60202">PREVELIKE</font> zalihe.
+            </p>
+
         </div>
     </div>
 
-    <!--- Three Column Section -->
-    <div class="container-fluid padding">
-        <div class="row text-center padding">
-            <div class="col-xs-12 col-sm-16 col-md-4">
-                <h3>A+</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>A-</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>B+</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>B-</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>0+</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>0-</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>AB+</h3>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <h3>AB-</h3>
-            </div>
-        </div>
+
+    <div>
+        <div id='supplies' onload></div>
+        <script>
+            $(function(){
+                $('#supplies').load('zalihe.php');
+            });
+        </script>
     </div>
+    <div style="width:70%;background-color:#e9ecef;padding:30px;border-radius:8px">
+        <p style="font-size: 19px">
+            Ako je trenutačna količina Vaše krvne grupe ispod oznake <font style="color: #A60202"> premale </font> zalihe, molimo Vas razmislite o donaciji!
+        </p>
+    </div>
+    <!--- Three Column Section -->
 
     <!--- Two Column Section -->
-    <div class="container-fluid padding">
+    <div class="container-fluid padding" style="padding-top: 50px">
         <div class="row padding">
             <div class="col-lg-6">
-                <h2>Lldnlnf</h2>
-                <p>slgnlsfgaieufnsdbvsdsuososogsougn</p>
-                <p>subgiwulebviuwvjdufrhuodčshuo</p>
-                <p>sdjfbisbsuihfuosdfhosuvsoubvousbvosufuoshfuosdhvuodbvouabvuosbvuosfbvuosf</p>
-                <br>
-                <a href="#" class="btn btn-primary">Learn More</a>
+                <img src="img/slika1.jpg" class="img-fluid" style="width:80%;border-radius:8px">
             </div>
-            <div class="col-lg-6">
-                <img src="img/slika1.jpg" class="img-fluid">
+            <div class="col-lg-6" style="margin-left:-130px;background-color:#e9ecef;border-radius:8px;height:300px;margin-top:16px;padding:30px">
+                <h1>Zašto donirati?</h1><br>
+                <p><font style="font-size: 18px">Opće je poznato da krv nije moguće proizvesti na umjetan način.
+                        Jedini izvor toga lijeka je čovjek - DONOR KRVI, a 1 doza krvi moze spasiti čak 3 života!</font></p>
+                <br>
+                <a href="indexdonacija.php" class="btn btn-primary">Saznaj više</a>
             </div>
         </div>
-    </div>
+    </div><br><br><br>
 
-    <!--- Fixed background -->
-
-
-    <!--- Emoji Section -->
 
 
     <!--- Meet the team -->
+    <div style="background-color: #e9ecef;">
     <div class="container-fluid padding">
         <div class="row welcome text-center">
             <div class="col-12">
-                <h1 class="display-4">Meet the hospitalsss</h1>
+                <h1 class="display-4">Kako doći do nas?</h1>
             </div>
         </div>
     </div>
@@ -159,100 +142,41 @@ mysqli_set_charset($conn,"utf8");
         <div class="row padding">
             <div class="col-md-4">
                 <div class="card">
-                    <img class="card-img-top" src="img/hospital.jpg">
+                    <img class="card-img-top" src="img/riteh.jpg" style="width:460px;height:300px">
                     <div class="card-body">
-                        <h4 class="card-title">KBC Rijeka</h4>
-                        <p class="card-text">PIifsibijfb</p>
-                        <a href="#" class="btn btn-outline-secondary">See Location</a>
+                        <h4 class="card-title">Glavni ured</h4>
+                        <p class="card-text">Vukovarska ul. 58</p>
+                        <a href="karta.php?naziv=Glavni ured&adresa=Vukovarska ul.58, Rijeka" class="btn btn-outline-secondary">Pokaži na karti</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img class="card-img-top" src="img/hospital.jpg">
+                    <img class="card-img-top" src="img/grobnik.jpg" style="width:460px;height:300px">
                     <div class="card-body">
-                        <h4 class="card-title">KBC Sušak</h4>
-                        <p class="card-text">PIifsibijfb</p>
-                        <a href="#" class="btn btn-outline-secondary">See Location</a>
+                        <h4 class="card-title">Poslovnica - Grobnik</h4>
+                        <p class="card-text">Dražičkih boraca 64</p>
+                        <a href="karta.php?naziv=Poslovnica - Grobnik&adresa=Dražičkih boraca 64, Dražice" class="btn btn-outline-secondary">Pokaži na karti</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <img class="card-img-top" src="img/hospital.jpg">
+                    <img class="card-img-top" src="img/benkovac.jpg" style="width:460px;height:300px">
                     <div class="card-body">
-                        <h4 class="card-title">KBC Zadar</h4>
-                        <p class="card-text">PIifsibijfb</p>
-                        <a href="#" class="btn btn-outline-secondary">See Location</a>
+                        <h4 class="card-title">Poslovnica - Benkovac</h4>
+                        <p class="card-text">Ante Starčević 10</p>
+                        <a href="karta.php?naziv=Poslovnica - Benkovac&adresa=Ante Starčević 10, Benkovac" class="btn btn-outline-secondary">Pokaži na karti</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <!--PopUp Sign in Form
-    <div id="modal-wrapper" class="modal">
-        <form class="modal-content animate" action="donor.php" method="GET">
-
-            <div class="imgcontainer">
-                <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close Popup">&times;</span>
-                <br><h1>Sign in</h1>
-            </div>
-
-            <div class="container">
-                <div style="padding-top: 40%;" class="textboxlogin">
-                    <i class="fas fa-user-circle"></i>
-                    <input type="text" placeholder="Username" name="username" value="" required="">
-                </div>
-
-                <div class="textboxlogin">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Password" name="password" value="" required="">
-                </div>
-                <br>
-                <input class="btnlogin" type="submit" name="submit" value="Prijavi se"><br>
-                <br><br>
-                <h6>Nemate račun?</h6> <a href="signup.php">Sign up</a>
-            </div>
-
-        </form>
     </div>
-
-    <script>
-    // Get the modal
-    var modal = document.getElementById('modal-wrapper');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    </script>-->
-
-    <!--- Two Column Section -->
-
-
-    <!--- Connect -->
-
-
-    <!--- Footer -->
-    <!--<footer>
-    <div class="container-fluid padding">
-    <div class="row text-center">
-        <div class="col-md-4">
-            <img src="img/logo_.png">
-            <hr class="light">
-            <h5>&</h5>
-        </div>
-    </div>
-    </div>
-    </footer>
-    -->
+</div>
 </div>
 </body>
 </html>
-
 
 
 
